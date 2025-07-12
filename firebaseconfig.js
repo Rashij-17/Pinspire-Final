92a883cac864665b30f60967dfa9308da3490ee4
Rashi Johari <rashijohari17@gmail.com>
2025-07-12 16:49:26 +0530

    Add files via upload

---
+++ firebaseconfig.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBXCwE7nPnlJzT50px5RLN9u_x_8c9uleQ",
+  authDomain: "pinspire-bcad1.firebaseapp.com",
+  projectId: "pinspire-bcad1",
+  storageBucket: "pinspire-bcad1.firebasestorage.app",
+  messagingSenderId: "1014574076859",
+  appId: "1:1014574076859:web:bdc292166b7f29fdcdaa9e"
+};
